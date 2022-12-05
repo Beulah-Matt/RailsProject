79d3ae8e97601930bbfa150fe5401c7efb452e8b
Beulah Mathenge <beulahmathenge@gmail.com>
2022-12-05 17:20:54 +0300

    Tailwind and Footer setup

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className = "font-bold text-green-500">App</div>
+  )
+}
+
+export default App
---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+
+export{
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+}
