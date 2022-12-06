fdae07375484a89aceb525b5dbdc30b57dbc38fe
Beulah Mathenge <beulahmathenge@gmail.com>
2022-12-06 20:19:47 +0300

    Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :email, uniqueness: true
+
     #Attaching rlship to User
     has_many :movie_collections
     has_many :movies, through: :movie_collections
