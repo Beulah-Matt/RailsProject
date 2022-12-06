edc31c29dc88a7cd2b59d9eef98b22d617ae3847
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-06 03:13:33 +0300

    Attaching rlship

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+    #Attaching rlship  in Movie
+    has_many :movie_collections
+    has_many :users, through: :movie_collections
 end
--- app/models/movie_collection.rb
+++ app/models/movie_collection.rb
@@ -1,2 +1,5 @@
 class MovieCollection < ApplicationRecord
+    #Attaching rlship to Movie collection
+    belongs_to: users
+    belongs_to: movies
 end
